5a0284e6b2720e0a8801e64c76b16d3b12e1fb1e
vjeyar <vignesh.jeyaraj@nike.com>
2017-04-05 15:52:13 -0700

    Removing the unwanted files.

--- src/test/java/service/ExporterSpec.java
+++ src/test/java/service/ExporterSpec.java
@@ -1,13 +1,12 @@
 package service;
 
 
-
 public class ExporterSpec {
 
     public static void main(String[] args)
     {
-        EmployeeServiceImpl webservice = new service.EmployeeServiceImplService().getEmployeeServiceImplPort();
-        service.Employee response = webservice.getEmpId(1);
-        System.out.println(response.toString());
+//        EmployeeServiceImpl webservice = new service.EmployeeServiceImplService().getEmployeeServiceImplPort();
+//        service.Employee response = webservice.getEmpId(1);
+//        System.out.println(response.toString());
     }
 }
